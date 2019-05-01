1df848ca9fd4a17c7218f5d769aeaa47fa2a1835
Mustafa Anas <ad16mkha@uwcad.it>
2019-05-02 02:04:52 +0300

    Server Initiated

--- client/components/SecondScreen.js
+++ client/components/SecondScreen.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+const styles = StyleSheet.create({
+    text: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+    }
+})
+
 export class DetailsScreen extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            text: ''
+        }
+    }
   render() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Details Screen</Text>
-            <Button
-                title="Go to Details... again"
-                onPress={() => this.props.navigation.push('Details')}
+            <TextInput
+                style={{height: 40}}
+                placeholder="Type here to translate!"
+                onChangeText={(text) => this.setState({text})}
             />
-            {/* <Button
-                title="Go to Home"
-                onPress={() => this.props.navigation.navigate('Home')}
-            /> */}
+            <View style={styles.text}>
+                <Text>{this.state.text}</Text>
+            </View>
             <Button
                 icon={
                 <Icon
@@ -27,10 +45,6 @@ export class DetailsScreen extends Component {
                 iconRight
                 title="Button with right icon"
                 onPress={() => this.props.navigation.navigate('Home')}
-            />
-            <Button
-                title="Go back"
-                onPress={() => this.props.navigation.goBack()}
             />
         </View>
     )
